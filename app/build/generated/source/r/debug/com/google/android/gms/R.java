4057e0d3c44351bee87634c07bd9bbe239f93c23
siya@azmediame.net <priya_m20@hotmail.com>
2017-05-10 00:33:22 +0400

    Merge remote-tracking branch 'remotes/origin/master' into HEAD
    # Conflicts:
    #	.gradle/3.3/taskArtifacts/fileHashes.bin
    #	.gradle/3.3/taskArtifacts/fileSnapshots.bin
    #	.gradle/3.3/taskArtifacts/taskArtifacts.bin
    #	.gradle/3.3/taskArtifacts/taskArtifacts.lock
    #	.gradle/3.3/tasks/_app_compileDebugJavaWithJavac/localClassSetAnalysis/localClassSetAnalysis.lock
    #	.gradle/3.3/tasks/_app_compileDebugJavaWithJavac/localJarClasspathSnapshot/localJarClasspathSnapshot.lock
    #	app/build/intermediates/blame/res/debug/multi/values.json
    #	app/build/intermediates/blame/res/debug/single/color.json
    #	app/build/intermediates/blame/res/debug/single/drawable-hdpi-v4.json
    #	app/build/intermediates/blame/res/debug/single/drawable-mdpi-v4.json
    #	app/build/intermediates/blame/res/debug/single/drawable-xhdpi-v4.json
    #	app/build/intermediates/blame/res/debug/single/drawable-xxhdpi-v4.json
    #	app/build/intermediates/blame/res/debug/single/drawable.json
    #	app/build/intermediates/blame/res/debug/single/mipmap-hdpi-v4.json
    #	app/build/intermediates/blame/res/debug/single/mipmap-mdpi-v4.json
    #	app/build/intermediates/blame/res/debug/single/mipmap-xhdpi-v4.json
    #	app/build/intermediates/blame/res/debug/single/mipmap-xxhdpi-v4.json
    #	app/build/intermediates/blame/res/debug/single/mipmap-xxxhdpi-v4.json
    #	app/build/intermediates/classes/debug/info/androidhive/loginandregistration/activity/Viewdoctor.class
    #	app/build/intermediates/incremental/mergeDebugAndroidTestResources/compile-file-map.properties
    #	app/build/intermediates/incremental/mergeDebugResources/compile-file-map.properties

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -80,7 +80,7 @@ public final class R {
         public static final int auto = 0x7f0f002d;
         public static final int center = 0x7f0f002f;
         public static final int dark = 0x7f0f004e;
-        public static final int email = 0x7f0f0099;
+        public static final int email = 0x7f0f009b;
         public static final int hybrid = 0x7f0f0043;
         public static final int icon_only = 0x7f0f004b;
         public static final int light = 0x7f0f004f;
@@ -90,8 +90,9 @@ public final class R {
         public static final int satellite = 0x7f0f0044;
         public static final int standard = 0x7f0f004c;
         public static final int terrain = 0x7f0f0045;
-        public static final int text = 0x7f0f00fd;
-        public static final int text2 = 0x7f0f00fb;
+        public static final int text = 0x7f0f0105;
+        public static final int text2 = 0x7f0f0103;
+        public static final int toolbar = 0x7f0f00a9;
         public static final int wide = 0x7f0f004d;
         public static final int wrap_content = 0x7f0f002c;
     }
